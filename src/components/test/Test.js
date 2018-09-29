7ef4dad109fc40d25a0d2d1ce03932901497f316
David Rout <nzdave@gmail.com>
2018-09-28 22:15:09 -0700

    initial commit

---
+++ src/components/test/Test.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+class Test extends Component {
+  state = {
+    title: "",
+    body: ""
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          title: data.title,
+          body: data.body
+        })
+      );
+  }
+
+  // componentWillMount() {
+  //   console.log("Component Will Mount");
+  // }
+
+  // componentDidUpdate() {
+  //   console.log("Component Did Update");
+  // }
+
+  // componentWillUpdate() {
+  //   console.log("Component Will Update");
+  // }
+
+  // componentWillReceiveProps(nextProps, nextState) {
+  //   console.log("Component WillRecieveProps");
+  // }
+
+  // static getDerivedStateFromProps(nextProps, prevState) {
+  //   return null;
+  // }
+
+  // getSnapshotBeforeUpdate(prevProps, prevState) {
+  //   console.log("getSnapShotBeforeUpdate");
+  // }
+
+  render() {
+    const { title, body } = this.state;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <p>{body}</p>
+      </div>
+    );
+  }
+}
+export default Test;
